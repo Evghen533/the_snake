b8024e92fee40f6c90370926a3c8beb7285c7a74
Евгений <Evghensokolov@yandex.ru>
2026-03-20 16:04:25 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -62,7 +62,10 @@ class Apple(GameObject):
     def __init__(self, occupied_slots: Optional[List[Tuple[int, int]]] = None) -> None:
         """Инициализация яблока."""
         super().__init__(body_color=APPLE_COLOR)
-        self.randomize_position(occupied_slots or [])
+        # Разбиваем длинную строку на две:
+        self.randomize_position(
+            occupied_slots or []
+        )
 
     def draw(self) -> None:
         """Отрисовка яблока."""
@@ -71,6 +74,7 @@ class Apple(GameObject):
     def randomize_position(self, occupied_slots: List[Tuple[int, int]]) -> None:
         """Генерация случайной позиции, не занятой змейкой."""
         while True:
+            # Разбиваем длинный кортеж:
             self.position = (
                 random.randint(0, GRID_WIDTH - 1) * GRID_SIZE,
                 random.randint(0, GRID_HEIGHT - 1) * GRID_SIZE
