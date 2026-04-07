569bffd2d028eeab21c885727d0bcc49fe0cce9a
Евгений <Evghensokolov@yandex.ru>
2026-04-07 15:46:59 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,7 @@
-import pytest
 import os
 
-# Принудительно отключаем окно для всех тестов
-os.environ['SDL_VIDEODRIVER'] = 'dummy'
+import pytest
+
 
 @pytest.fixture
 def _the_snake():
@@ -10,28 +9,40 @@ def _the_snake():
     import the_snake
     return the_snake
 
+
 @pytest.fixture
 def game_object(_the_snake):
     """Фикстура для базового объекта."""
     return _the_snake.GameObject()
 
+
 @pytest.fixture
 def apple(_the_snake):
-    """Фикстура для яблока (нужна тестам!)."""
+    """Фикстура для яблока."""
     return _the_snake.Apple()
+
 
 @pytest.fixture
 def snake(_the_snake):
-    """Фикстура для змейки (нужна тестам!)."""
+    """Фикстура для змейки."""
     return _the_snake.Snake()
 
+
 class StopInfiniteLoop(Exception):
+    """Исключение для остановки бесконечного цикла."""
+
     pass
+
 
 @pytest.fixture(autouse=True)
 def stop_main_loop(monkeypatch):
     """Фикстура для остановки бесконечного цикла."""
     import pygame
+
     def mock_update():
         raise StopInfiniteLoop
-    monkeypatch.setattr(pygame.display, "update", mock_update)
+    monkeypatch.setattr(pygame.display, 'update', mock_update)
+
+
+# Принудительно отключаем окно для всех тестов
+os.environ['SDL_VIDEODRIVER'] = 'dummy'
