87d012a9119a44f730ee61be2b3916f0b7d44c36
Евгений <Evghensokolov@yandex.ru>
2026-04-07 16:04:23 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,4 @@
 import pytest
-# Импортируем наше исключение, чтобы тест его "узнал"
 from conftest import StopInfiniteLoop
 from the_snake import main
 
@@ -9,12 +8,8 @@ def test_main_run_without_exceptions():
     try:
         main()
     except StopInfiniteLoop:
-        # Если поймали это исключение — значит, main() дошел до отрисовки
-        # и успешно начал работать. Это победа.
         pass
     except SystemExit:
-        # Если вдруг сработал выход из системы
         pass
     except Exception as e:
-        # Любая другая ошибка всё еще завалит тест
         pytest.fail(f'При запуске функции main возникло исключение: {e}')
