1e6b75afb2b2423b42c5d0b74eae883d2fd0cde1
Евгений <Evghensokolov@yandex.ru>
2026-04-07 18:27:20 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,20 @@
 import pytest
+
 from conftest import StopInfiniteLoop
 from the_snake import main
+
 
 def test_main_run_without_exceptions():
     """Проверка запуска главного цикла без ошибок."""
     try:
         main()
     except StopInfiniteLoop:
-        # ПЕРЕХВАТЫВАЕМ ТУТ: это значит, всё хорошо, игра запустилась!
+        # Если поймали это исключение — значит, main() дошел до отрисовки
+        # и успешно начал работать. Это и есть прохождение теста.
         pass
     except SystemExit:
+        # На случай, если сработал выход через pygame.quit()
         pass
     except Exception as e:
-        # Если ошибка НЕ StopInfiniteLoop, тогда валим тест
+        # Любая другая ошибка всё еще должна завалить тест
         pytest.fail(f'При запуске функции main возникло исключение: {e}')
