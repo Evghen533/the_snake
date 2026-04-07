b398411146a592a9366b85e0b3434e827c57b945
Евгений <Evghensokolov@yandex.ru>
2026-04-07 16:26:00 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,14 +3,12 @@ from the_snake import main
 
 
 def test_main_run_without_exceptions():
-    """Проверка запуска главного цикла без ошибок."""
+    """Проверка запуска главного цикла."""
     try:
         main()
     except Exception as e:
-        # Если это наша остановка StopInfiniteLoop - всё отлично, выходим
         if 'StopInfiniteLoop' in str(type(e)):
             return
         pytest.fail(f'При запуске функции main возникло исключение: {e}')
     except BaseException:
-        # Перехватываем системные выходы
         pass
