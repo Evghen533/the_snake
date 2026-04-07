a09ed44918dcdc006745feb2e0824352cf7288ba
Евгений <Evghensokolov@yandex.ru>
2026-04-07 15:59:14 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,19 @@
 import pytest
+# Импортируем наше исключение из conftest
 from conftest import StopInfiniteLoop
 from the_snake import main
 
 
 def test_main_run_without_exceptions():
-    """Проверка запуска главного цикла без ошибок."""
+    """Проверка запуска главного цикла."""
     try:
         main()
     except StopInfiniteLoop:
-        # Это нормальное поведение: тест сам остановил цикл
+        # Это значит, что фикстура успешно остановила бесконечный цикл
         pass
     except SystemExit:
-        # Если pygame.quit() сработал
+        # Это если сработал sys.exit()
         pass
     except Exception as e:
+        # А вот любая другая ошибка — это уже повод завалить тест
         pytest.fail(f"При запуске функции main возникло исключение: {e}")
