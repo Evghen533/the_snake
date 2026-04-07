dda8447f0e174b9d380f20b7dae7a4454b9d8f29
Евгений <Evghensokolov@yandex.ru>
2026-04-07 17:41:55 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,4 +17,3 @@ def test_main_run_without_exceptions():
     except Exception as e:
         # Любая другая реальная ошибка завалит тест
         pytest.fail(f'При запуске функции main возникло исключение: {e}')
-
