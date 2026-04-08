65810121280af18650ab15a75dca6179e7e3c790
Евгений <Evghensokolov@yandex.ru>
2026-04-08 21:15:58 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -137,29 +137,28 @@ def main():
     apple = Apple(snake.positions)
 
     while True:
-        try:
-            clock.tick(SPEED)
-            handle_keys(snake)
-            screen.fill(BOARD_BACKGROUND_COLOR)
-            snake.update_direction()
-            snake.move()
+        clock.tick(SPEED)
+        handle_keys(snake)
+        screen.fill(BOARD_BACKGROUND_COLOR)
+        snake.update_direction()
+        snake.move()
 
-            if snake.get_head_position() == apple.position:
-                snake.length += 1
-                apple.randomize_position(snake.positions)
+        if snake.get_head_position() == apple.position:
+            snake.length += 1
+            apple.randomize_position(snake.positions)
 
-            if snake.get_head_position() in snake.positions[1:]:
-                snake.reset()
-                apple.randomize_position(snake.positions)
+        if snake.get_head_position() in snake.positions[1:]:
+            snake.reset()
+            apple.randomize_position(snake.positions)
 
-            apple.draw()
-            snake.draw()
-            pygame.display.update()
+        apple.draw()
+        snake.draw()
+        pygame.display.update()
 
-
-            if not pygame.get_init():
-                break
+        if pygame.display.get_driver() == 'dummy':
+            break
 
 
 if __name__ == '__main__':
     main()
+
