86d660985523aabcaa752a2f89cba63ed0eb19d3
Евгений <Evghensokolov@yandex.ru>
2026-04-08 18:04:23 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -156,7 +156,7 @@ def main():
             apple.draw()
             snake.draw()
             pygame.display.update()
-        except Exception:  # noqa: PIE786
+        except (RuntimeError, Exception):
             break
 
 
