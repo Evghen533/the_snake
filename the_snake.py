022a686d0379c2cb18efbffaad16316f01d7f041
Евгений <Evghensokolov@yandex.ru>
2026-04-08 19:28:03 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -119,7 +119,7 @@ def handle_keys(game_object):
     """Обработка клавиш управления."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            pass
             raise SystemExit
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and game_object.direction != DOWN:
