06523280a16328338f2e3f515d48d6e8000478e1
Евгений <Evghensokolov@yandex.ru>
2026-04-11 16:54:44 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -161,6 +161,8 @@ def main():
             pygame.display.update()
         except (KeyboardInterrupt, SystemExit, StopInfiniteLoop):
             break
+        except Exception:  # noqa: PIE786
+            break
 
 
 if __name__ == '__main__':
