9fa3c841930c46289da8d01fbab64d4f3fea8953
Евгений <Evghensokolov@yandex.ru>
2026-04-11 17:30:08 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -162,10 +162,8 @@ def main():
         except (KeyboardInterrupt, SystemExit, StopInfiniteLoop):
             break
         except BaseException as e:
-            # Если имя класса совпало, значит это исключение от pytest
             if e.__class__.__name__ == 'StopInfiniteLoop':
                 break
-            # Если это другая ошибка — пробрасываем её дальше (для PIE786)
             raise e
 
 
