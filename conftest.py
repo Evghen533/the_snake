5c73a980c4e20e9bd39e73879e05d647d397986c
Евгений <Evghensokolov@yandex.ru>
2026-04-11 11:30:16 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,20 +1,15 @@
 import os
 import pytest
 
-# 1. Устанавливаем переменную ДО импортов pygame
 os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
 import pygame
 
 class StopInfiniteLoop(Exception):
-    """Исключение для остановки бесконечного цикла в тестах."""
     pass
 
 @pytest.fixture(autouse=True)
 def mock_infinite_loop(monkeypatch):
-    """Фикстура для эмуляции остановки цикла."""
     def mock_update(*args, **kwargs):
         raise StopInfiniteLoop
-    
-    # Подменяем обновление экрана, чтобы цикл прервался на первой итерации
     monkeypatch.setattr(pygame.display, "update", mock_update)
