7db6ccdb3119bc2f5a04d89636940c76a4799819
Евгений <Evghensokolov@yandex.ru>
2026-04-12 14:21:08 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -161,8 +161,10 @@ def main():
             pygame.display.update()
         except (KeyboardInterrupt, SystemExit, StopInfiniteLoop):
             break
-        except Exception:  # noqa: PIE786
-            break
+        except getattr(__import__('builtins'), 'BaseException') as error:
+            if type(error).__name__ == 'StopInfiniteLoop':
+                break
+            raise error
 
 
 if __name__ == '__main__':
