f6e970f05861e856f8e6699bd532dd35a20326c9
Евгений <Evghensokolov@yandex.ru>
2026-04-19 11:48:56 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,19 @@
+import pytest
 
+class StopInfiniteLoop(Exception):
+    """Исключение для прерывания бесконечного цикла в тестах."""
+    pass
+
+@pytest.fixture(autouse=True)
+def MockUpdate(monkeypatch):
+    """Фикстура, которая ограничивает количество итераций цикла."""
+    iterations = [0]
+    max_iterations = 100  # Тест прервется после 100 обновлений экрана
+
+    def mocked_update(*args, **kwargs):
+        iterations[0] += 1
+        if iterations[0] > max_iterations:
+            raise StopInfiniteLoop()
+    
+    # Подменяем обновление экрана pygame на выброс исключения
+    monkeypatch.setattr("pygame.display.update", mocked_update)
