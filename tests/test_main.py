90c1c645c3b523db57bb707dc619a2fd2f929eba
Евгений <Evghensokolov@yandex.ru>
2026-04-24 20:06:33 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,18 @@
 import pytest
 
+# Важно: импортируем именно из conftest
 from conftest import StopInfiniteLoop
 from the_snake import main
 
 
 def test_main_run_without_exceptions():
-    """Проверка main на корректный выход по исключению."""
+    """Тест проверяет, что main корректно завершается по требованию."""
     try:
         main()
     except StopInfiniteLoop:
-        # Тест пройдет, если поймает это специфичное исключение
+        # Если поймали это исключение — тест пройден успешно
+        pass
+    except SystemExit:
         pass
     except Exception as e:
         pytest.fail(f'При запуске функции main возникло исключение: {e}')
