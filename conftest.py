deaf7021d68320f1070e7035cf9de212fc07b60d
Евгений <Evghensokolov@yandex.ru>
2026-04-24 17:25:57 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,12 @@
 import pytest
 
 
+@pytest.fixture
+def sample_numbers():
+    """Возвращает кортеж с числами для тестирования"""
+    return (5, 10, 15)
+
+
 class StopInfiniteLoop(Exception):
     """Исключение для прерывания бесконечного цикла в тестах."""
 
