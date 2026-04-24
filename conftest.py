16d2828489ff4f158fb1acf0890d72bce5ef350e
Евгений <Evghensokolov@yandex.ru>
2026-04-24 20:42:19 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,14 +2,14 @@ import pytest
 
 
 class StopInfiniteLoop(Exception):
-    """Исключение для остановки цикла."""
+    """Исключение для прерывания бесконечного цикла в тестах."""
 
     pass
 
 
 @pytest.fixture(autouse=True)
 def mock_update(monkeypatch):
-    """Подменяет update для выхода из цикла."""
+    """Фикстура для ограничения итераций цикла."""
     def mocked_update(*args, **kwargs):
         raise StopInfiniteLoop()
 
