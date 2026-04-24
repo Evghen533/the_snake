1a9d0cd0bd1e3e74a86e100f2866f9c5d17e33ac
Евгений <Evghensokolov@yandex.ru>
2026-04-24 20:34:31 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,17 @@
 import pytest
-import pygame
 
 from the_snake import main
 
 
 def test_main_run_without_exceptions():
-    """Проверка main на корректный выход по исключению."""
+    """Тест проверяет, что main корректно завершается по требованию."""
     try:
         main()
     except Exception as e:
-        # Если название ошибки StopInfiniteLoop — значит всё круто, цикл прерван
+        # Проверяем имя исключения как строку.
+        # Это сработает, даже если объекты классов в памяти не совпали.
         if type(e).__name__ == 'StopInfiniteLoop':
             return
-        # Если это выход из системы — тоже Ок
         if isinstance(e, SystemExit):
             return
-        # А вот если что-то другое — тогда заваливаем тест
         pytest.fail(f'При запуске функции main возникло исключение: {e}')
