4074d48dd566759d543afa76893c1e3f1a1ab199
Евгений <Evghensokolov@yandex.ru>
2026-05-17 11:36:58 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,9 @@ def add_sum(a, b):
 
 
 def test_add():
-    assert add_sum(2, 3) == 5, 'сумма не равна ожидаемой'
+    assert add_sum(3, 3) == 5, 'сумма не равна ожидаемой' # Сделали несоответствие итоговому результату
 
 
 def test_type_result():
-    assert isinstance(add_sum(2, 3), int), \
-        'не соответствует ожидаемому типу данных'
+    assert isinstance(add_sum(2, 3.5), int), \
+        'не соответствует ожидаемому типу данных' # а тут мы уже получим float, а не int.
